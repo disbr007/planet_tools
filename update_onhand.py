9383849fa1a2f80dd9175b140089a66a88ab7855
disbr007 <disbr007@umn.edu>
2020-08-06 09:49:18 -0500

    init for finding onhand scenes and updating table

--- update_onhand.py
+++ update_onhand.py
@@ -7,10 +7,8 @@ import geopandas as gpd
 from logging_utils.logging_utils import create_logger, create_logfile_path
 from db_utils import Postgres
 
-
 logger = create_logger(__name__, 'sh', 'INFO')
 # logger = create_logger(__name__, 'fh', 'DEBUG',
                        # filename=create_logfile_path(Path(__file__).stem))
 
-create_logfile_path(Path(__file__).stem)
 
