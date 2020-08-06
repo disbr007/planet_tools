a3b8d119ea05017fd7ddab4e419868570be896aa
disbr007 <disbr007@umn.edu>
2020-08-06 09:48:20 -0500

    created to hold basic functions like getting config params

--- lib.py
+++ lib.py
@@ -2,11 +2,17 @@ import os
 import pathlib
 import json
 
-config_file = os.path.join(os.path.dirname(__file__),"config", "aws_creds.json")
+config_file = os.path.join(os.path.dirname(__file__), "config", "config.json")
 
 
 def get_config(param):
     config_params = json.load(open(config_file))
-    config = config_params[param]
+    try:
+        config = config_params[param]
+    except KeyError:
+        print('Config parameter not found: {}'.format(param))
 
     return config
+
+
+
