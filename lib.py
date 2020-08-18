bf3aca33fdec21989c770dc77f30407df08e1526
disbr007 <disbr007@umn.edu>
2020-08-18 15:51:11 -0500

    Added read_ids and write_gdf fxns

--- lib.py
+++ lib.py
@@ -1,18 +1,143 @@
+import copy
 import os
-import json
+import sys
+from pathlib import Path
+import pathlib
+
+import pandas as pd
+import geopandas as gpd
+
+from logging_utils.logging_utils import create_logger
+
+logger = create_logger(__name__, 'sh', 'INFO')
 
 
-config_file = os.path.join(os.path.dirname(__file__), "config", "config.json")
+def type_parser(filepath):
+    '''
+    takes a file path (or dataframe) in and determines whether it is a dbf,
+    excel, txt, csv (or df), ADD SUPPORT FOR SHP****
+    '''
+    if isinstance(filepath, pathlib.PurePath):
+        fp = str(filepath)
+    else:
+        fp = copy.deepcopy(filepath)
+    if type(fp) == str:
+        ext = os.path.splitext(fp)[1]
+        if ext == '.csv':
+            with open(fp, 'r') as f:
+                content = f.readlines()
+                for row in content[0]:
+                    if len(row) == 1:
+                        file_type = 'id_only_txt' # txt or csv with just ids
+                    elif len(row) > 1:
+                        file_type ='csv' # csv with columns
+                    else:
+                        logger.error('Error reading number of rows in csv.')
+        elif ext == '.txt':
+            file_type = 'id_only_txt'
+        elif ext in ('.xls', '.xlsx'):
+            file_type = 'excel'
+        elif ext == '.dbf':
+            file_type = 'dbf'
+        elif ext == '.shp':
+            file_type = 'shp'
+    elif isinstance(fp, (gpd.GeoDataFrame, pd.DataFrame)):
+        file_type = 'df'
+    else:
+        logger.error('Unrecognized file type. Type: {}'.format(type(fp)))
 
-def get_config(param):
-    config_params = json.load(open(config_file))
-    try:
-        config = config_params[param]
-    except KeyError:
-        print('Config parameter not found: {}'.format(param))
-
-    return config
+    return file_type
 
 
+def read_ids(ids_file, field=None, sep=None, stereo=False):
+    '''Reads ids from a variety of file types. Can also read in stereo ids from applicable formats
+    field: field name, irrelevant for text files, but will search for this name if ids_file is .dbf or .shp
+    '''
+
+    # Determine file type
+    file_type = type_parser(ids_file)
+
+    if file_type in ('dbf', 'df', 'gdf', 'shp', 'csv', 'excel') and not field:
+        logger.error('Must provide field name with file type: {}'.format(file_type))
+        sys.exit()
+
+    # Text file
+    if file_type == 'id_only_txt':
+        ids = []
+        with open(ids_file, 'r') as f:
+            content = f.readlines()
+            for line in content:
+                if sep:
+                    # Assumes id is first
+                    the_id = line.split(sep)[0]
+                    the_id = the_id.strip()
+                else:
+                    the_id = line.strip()
+                ids.append(the_id)
+                
+    # csv
+    elif file_type in ('csv'):
+        df = pd.read_csv(ids_file, sep=sep,)
+        ids = list(df[field])
+
+    # dbf, gdf, dbf
+    elif file_type in ('dbf'):
+        df = gpd.read_file(ids_file)
+        ids = list(df[field])
+
+    # SHP
+    elif file_type == 'shp':
+        df = gpd.read_file(ids_file)
+        ids = list(df[field])
+
+    # GDF, DF
+    elif file_type in ('gdf', 'df'):
+        ids = list(ids_file[field])
+
+    # Excel
+    elif file_type == 'excel':
+        df = pd.read_excel(ids_file, squeeze=True)
+        ids = list(df[field])
+
+    else:
+        logger.error('Unsupported file type... {}'.format(file_type))
+
+    return ids
 
 
+def remove_dt_gdf(gdf, date_format='%Y-%m-%d %H:%M:%S'):
+    # Convert datetime columns to str
+    date_cols = gdf.select_dtypes(include=['datetime64']).columns
+    for dc in date_cols:
+        gdf[dc] = gdf[dc].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+def write_gdf(gdf, out_footprint, out_format=None, date_format=None):
+    if not isinstance(out_footprint, pathlib.PurePath):
+        out_footprint = Path(out_footprint)
+
+    # Remove datetime - specifiy datetime if desired format
+    if not gdf.select_dtypes(include=['datetime64']).columns.empty:
+        remove_dt_gdf(gdf, date_format=date_format)
+    logger.info('Writing to file: {}'.format(out_footprint))
+    if not out_format:
+        out_format = out_footprint.suffix.replace('.', '')
+        if not out_format:
+            # If still no extension, check if gpkg (package.gpkg/layer)
+            out_format = out_footprint.parent.suffix
+            if not out_format:
+                logger.error('Could not recognize out format from file extension: {}'.format(out_footprint))
+
+    # Write out in format specified
+    if out_format == 'shp':
+        gdf.to_file(out_footprint)
+    elif out_format == 'geojson':
+        gdf.to_file(out_footprint,
+                    driver='GeoJSON')
+    elif out_format == 'gpkg':
+        gdf.to_file(out_footprint.parent, layer=out_footprint.stem,
+                    driver='GPKG')
+    else:
+        logger.error('Unrecognized format: {}'.format(out_format))
+
+    logger.info('Done.')
